0b48ab9b163593aed88c3da49a732493d83bad64
zhenhao goh <kenneth.gzhao@outlook.com>
2017-04-26 16:53:01 +0800

    class done

--- lib/bluray.rb
+++ lib/bluray.rb
@@ -1,4 +1,14 @@
 require_relative 'item.rb'
 
 class Bluray < Item
+
+  def initialize(name, price, runtime, director, producer)
+
+    super(name, price)
+    @runtime = runetime
+    @director = director
+    @producer = producer
+    
+  end
+
 end
--- lib/cd.rb
+++ lib/cd.rb
@@ -1,4 +1,12 @@
 require_relative 'item.rb'
 
 class Cd < Item
+
+  def initialize(name, price, tracks, artist, runtime)
+
+    super(name, price)
+    @tracks = tracks
+    @artist = artist
+    @runtime = runtime
+  end
 end
